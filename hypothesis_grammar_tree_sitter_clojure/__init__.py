2b106b376a45cefd7c413d928f227de835b1fd87
sogaiu <983021772@users.noreply.github.com>
2020-09-26 20:23:19 +0900

    Add more planning comments

--- hypothesis_grammar_tree_sitter_clojure/__init__.py
+++ hypothesis_grammar_tree_sitter_clojure/__init__.py
@@ -4,9 +4,18 @@ __version__ = '0.0.1'
 
 # XXX: could have assertions for checking has_error?
 
+# XXX: may be py-tree-sitter doesn't provide access to is_missing?
+
 # XXX: numeric constants being used in strategies are hard-wired -> change?
 
 # XXX: label strings could be factored out (e.g. "map", "number", etc.)
+
+# XXX: whether to test a variety of "separators" -- e.g. instead of just
+#      a single space:
+#
+#      * different types and amounts of whitespace,
+#      * discard expression
+#      * line comment (+ newline, if that's not considered part of comment)
 
 # grammatical pieces
 #
