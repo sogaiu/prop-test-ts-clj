ef1a9278f5aacee448ba0da4e157326130f3b848
sogaiu <983021772@users.noreply.github.com>
2020-09-28 21:37:44 +0900

    Update notes

--- hypothesis_grammar_tree_sitter_clojure/__init__.py
+++ hypothesis_grammar_tree_sitter_clojure/__init__.py
@@ -32,6 +32,10 @@ __version__ = '0.0.1'
 
 # XXX: look into the "recursive" and "data" strategies
 
+# XXX: split prop-test-ts-clj.py into pieces to make running individual /
+#      sets of tests easier?  currently a lot of commenting / uncommenting
+#      takes place
+
 # grammatical pieces
 #
 # subatomic (1)
