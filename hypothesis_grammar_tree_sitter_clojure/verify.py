ed36a07c8e6fd40a07ed6a1c29ea65fcb9e6109b
sogaiu <983021772@users.noreply.github.com>
2020-10-04 10:03:42 +0900

    Remove potentially stale comments + alpha

--- hypothesis_grammar_tree_sitter_clojure/verify.py
+++ hypothesis_grammar_tree_sitter_clojure/verify.py
@@ -41,21 +41,9 @@ def verify_node_type_and_text(ctx, item):
     return verify_node_type(ctx, item) and \
         verify_node_text(ctx, item)
 
-# (source [0, 0] - [1, 0]
-#   (keyword [0, 0] - [0, 2]))
-
-# (source [0, 0] - [1, 0]
-#   (symbol [0, 0] - [0, 6]))
-
 def verify_node_as_atom(ctx, item):
     return verify_node_type(ctx, item) and \
         verify_node_text(ctx, item)
-
-# (source [0, 0] - [1, 0]
-#   (vector [0, 0] - [0, 10]
-#     value: (keyword [0, 1] - [0, 3])
-#     value: (keyword [0, 4] - [0, 6])
-#     value: (keyword [0, 7] - [0, 9])))
 
 def verify_node_as_coll(ctx, coll_item):
     node, source = itemgetter('node', 'source')(ctx)
@@ -79,17 +67,6 @@ def verify_node_as_form(ctx, form_item):
     return verify_node_type(ctx, form_item) and \
         verify_node_text(ctx, form_item)
 
-# (source [0, 0] - [1, 0]
-#  (quote_form [0, 0] - [0, 12]
-#    value: (symbol [0, 1] - [0, 12])))
-
-# (source [0, 0] - [1, 0]
-#   (quote_form [0, 0] - [0, 19]
-#     value: (list [0, 1] - [0, 19]
-#       value: (symbol [0, 2] - [0, 6])
-#       value: (symbol [0, 7] - [0, 13])
-#       value: (symbol [0, 14] - [0, 18]))))
-
 def verify_node_as_adorned(ctx, adorned_item):
     node, source = itemgetter('node', 'source')(ctx)
     form_item, adorned_label = itemgetter('inputs', 'label')(adorned_item)
@@ -105,45 +82,6 @@ def verify_node_as_adorned(ctx, adorned_item):
                         form_item)
     return True
 
-# XXX: note that there is no value field for this case,
-#      but for all(?) other cases there is
-#
-# (source [0, 0] - [1, 0]
-#   (symbol [0, 0] - [0, 12]
-#     metadata: (metadata [0, 0] - [0, 5]
-#       (keyword [0, 1] - [0, 5]))))
-
-# (source [0, 0] - [1, 0]
-#   (vector [0, 0] - [0, 12]
-#     metadata: (metadata [0, 0] - [0, 7]
-#       (map [0, 1] - [0, 7]
-#         value: (keyword [0, 2] - [0, 4])
-#         value: (number [0, 5] - [0, 6])))
-#     value: (keyword [0, 9] - [0, 11])))
-
-# (source [0, 0] - [1, 0]
-#   (quote_form [0, 0] - [0, 14]
-#     metadata: (metadata [0, 0] - [0, 5]
-#       (keyword [0, 1] - [0, 5]))
-#     value: (vector [0, 7] - [0, 14]
-#       value: (keyword [0, 8] - [0, 10])
-#       value: (keyword [0, 11] - [0, 13]))))
-
-# (source [0, 0] - [1, 0]
-#   (quote_form [0, 0] - [0, 10]
-#     metadata: (metadata [0, 0] - [0, 5]
-#       (keyword [0, 1] - [0, 5]))
-#     value: (symbol [0, 7] - [0, 10])))
-
-# once a node with field "metadata" is found:
-
-# (metadata [0, 0] - [0, 7]
-#   (map [0, 1] - [0, 7]
-#     value: (keyword [0, 2] - [0, 4])
-#     value: (number [0, 5] - [0, 6])))
-
-# (metadata [0, 0] - [0, 5]
-#   (keyword [0, 1] - [0, 5]))
 
 def verify_node_metadata(ctx, item):
     node, source = itemgetter('node', 'source')(ctx)
@@ -197,52 +135,11 @@ def make_single_verifier(single_name):
         return single_item["verify"](single_ctx, single_item)
     return verifier
 
-# #::{}
-#
-# (source [0, 0] - [1, 0]
-#   (namespaced_map [0, 0] - [0, 5]
-#     prefix: (auto_res_marker [0, 1] - [0, 3])))
-
-# #::{:a 1}
-#
-# (source [0, 0] - [1, 0]
-#   (namespaced_map [0, 0] - [0, 9]
-#     prefix: (auto_res_marker [0, 1] - [0, 3])
-#     value: (keyword [0, 4] - [0, 6])
-#     value: (number [0, 7] - [0, 8])))
-
-# #:hello{}
-#
-# (source [0, 0] - [1, 0]
-#   (namespaced_map [0, 0] - [0, 9]
-#     prefix: (keyword [0, 1] - [0, 7])))
-
-# #:hello{:a 1}
-
-# (source [0, 0] - [1, 0]
-#   (namespaced_map [0, 0] - [0, 13]
-#     prefix: (keyword [0, 1] - [0, 7])
-#     value: (keyword [0, 8] - [0, 10])
-#     value: (number [0, 11] - [0, 12])))
-
-# #:hello/there{:a 1}
-#
-# (source [0, 0] - [1, 0]
-#   (namespaced_map [0, 0] - [0, 19]
-#     prefix: (keyword [0, 1] - [0, 13])
-#     value: (keyword [0, 14] - [0, 16])
-#     value: (number [0, 17] - [0, 18])))
-
 verify_node_prefix = make_single_verifier("prefix")
 
 def verify_node_with_prefix(ctx, item):
     return verify_node_prefix(ctx, item) and \
         verify_node_as_coll(ctx, item)
-
-# (source [0, 0] - [1, 0]
-#   (tagged_literal [0, 0] - [0, 44]
-#     tag: (symbol [0, 1] - [0, 5])
-#     value: (string [0, 6] - [0, 44])))
 
 verify_node_tag = make_single_verifier("tag")
 
@@ -253,16 +150,8 @@ def verify_node_with_tag(ctx, item):
         verify_node_as_adorned(ctx, item)
 
 # XXX: incomplete
-#def verify_node_as_form(ctx, item):
-#    return True
-
-# XXX: incomplete
 def verify_node_leads_with(ctx, item):
     return True
-
-# (source [0, 0] - [1, 0]
-#   (discard_expr [0, 0] - [0, 4]
-#     value: (number [0, 3] - [0, 4])))
 
 def verify_node_as_discard_expr(ctx, item):
     return verify_node_leads_with(ctx, item) and \
