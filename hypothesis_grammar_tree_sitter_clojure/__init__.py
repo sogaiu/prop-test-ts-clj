1abf7047fea618b35b15bb6c1181fabf64b55212
sogaiu <983021772@users.noreply.github.com>
2020-10-06 18:12:32 +0900

    Update notes

--- hypothesis_grammar_tree_sitter_clojure/__init__.py
+++ hypothesis_grammar_tree_sitter_clojure/__init__.py
@@ -10,19 +10,13 @@ __version__ = '0.0.1'
 # XXX: create clojure samples so that s-expression output can be
 #      programmatically generated from them when the grammar changes.
 
-# XXX: consider approach of flattening or converting tree-sitter tree
-#      to some other structure for comparison with expected information.
-#      expected information having an analogous structure might
-#      make comparison easier, so that might entail flattening or
-#      converting such info as well (post-generation).
-
 # XXX: if node.text gets merged in py-tree-sitter and it's usable,
 #      consider moving over to that instead of using node_text.
 #      n.b. that may require switching from strings to bytes --
 #      i.e. use b'hello' instead of 'hello'
 
-# XXX: could have assertions for checking that has_error does not
-#      apply
+# XXX: figure out where else has_error could be applied when
+#      verifying nodes
 
 # XXX: py-tree-sitter now has is_missing, so could verify that
 #      resulting nodes are not "is_missing"
@@ -32,15 +26,10 @@ __version__ = '0.0.1'
 #      make changing them later easier (e.g. as a consequence of
 #      changing the tree-sitter grammar "grammar symbols")
 
-# XXX: test a variety of "separators" -- e.g. instead of just
-#      a single space:
-#
-#      * different types and amounts of whitespace,
-#      * discard expression
-#      * line comment (+ newline, if that's not considered part of comment)
-#
-#      once the separators strategies is built, need to go around
-#      "deploying" it.
+# XXX: make separator-related bits more tweakable.  start testing with
+#      single whitespace and only after things working, make more fancy.
+
+# XXX: find places where separators strategies can / should be applied.
 #
 #      this may lead to changes in the various verify_* functions
 #      as "comment" and "discard_expr" nodes will start showing up.
@@ -49,11 +38,17 @@ __version__ = '0.0.1'
 #      basically the same sorts of things as for separators can go before
 #      or after things...need to track down possible locations
 
-# XXX: look into the "recursive", "deferred", and "data" strategies
+# XXX: look into the "deferred" and "data" strategies
 
 # XXX: split prop-test-ts-clj.py into pieces to make running individual /
 #      sets of tests easier?  currently a lot of commenting / uncommenting
 #      takes place
+
+# XXX: consider approach of flattening or converting tree-sitter tree
+#      to some other structure for comparison with expected information.
+#      expected information having an analogous structure might
+#      make comparison easier, so that might entail flattening or
+#      converting such info as well (post-generation).
 
 # grammatical pieces
 #
