15c59f053cbdad47d7d6ffb21fffc068040ffe39
sogaiu <983021772@users.noreply.github.com>
2020-10-10 17:17:23 +0900

    Minor renaming

--- prop-test-ts-clj.py
+++ prop-test-ts-clj.py
@@ -428,7 +428,7 @@ def test_parses_atom_vector_with_metadata(atom_vector_with_metadata_item):
 @settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
                                                HealthCheck.filter_too_much])
 @given(anon_func_with_metadata_items())
-def test_parses_anon_func_with_metadata(anon_func_with_metadata_item):
+def test_parses_anonymous_function_with_metadata(anon_func_with_metadata_item):
     form_test(anon_func_with_metadata_item)
 
 @settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
@@ -573,7 +573,7 @@ if __name__ == "__main__":
     test_parses_atom_namespaced_map_with_metadata()
     test_parses_atom_set_with_metadata()
     test_parses_atom_vector_with_metadata()
-    test_parses_anon_func_with_metadata()
+    test_parses_anonymous_function_with_metadata()
     test_parses_deref_form_with_metadata()
     test_parses_eval_form_with_metadata()
     test_parses_quote_form_with_metadata()
