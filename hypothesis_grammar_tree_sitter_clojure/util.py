ff20ab167b1b9db19c2d6dcc7caf72540db821d2
sogaiu <983021772@users.noreply.github.com>
2020-10-10 15:15:49 +0900

    Minor renaming

--- hypothesis_grammar_tree_sitter_clojure/util.py
+++ hypothesis_grammar_tree_sitter_clojure/util.py
@@ -3,10 +3,10 @@ def make_form_with_metadata_str_builder(base_str_builder):
         # avoid circular dependency
         from .metadata import attach_metadata
         #
-        vec_str = base_str_builder(item)
+        form_str = base_str_builder(item)
         #
         md_items = item["metadata"]
         md_item_strs = [md_item["to_str"](md_item) for md_item in md_items]
         #
-        return attach_metadata(md_item_strs, vec_str)
+        return attach_metadata(md_item_strs, form_str)
     return builder
