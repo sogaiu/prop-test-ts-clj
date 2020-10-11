a7c7293b9667a360d7b0b776464078c3fd13585b
sogaiu <983021772@users.noreply.github.com>
2020-10-11 19:47:41 +0900

    Minor renaming

--- hypothesis_grammar_tree_sitter_clojure/keywords.py
+++ hypothesis_grammar_tree_sitter_clojure/keywords.py
@@ -26,44 +26,44 @@ def build_kwd_str(item):
 
 @composite
 def unqualified_auto_resolved_keyword_items(draw):
-    a_kwd_str = draw(unqualified_auto_resolved_keyword_as_str())
+    kwd_str = draw(unqualified_auto_resolved_keyword_as_str())
     #
-    return {"inputs": a_kwd_str,
+    return {"inputs": kwd_str,
             "label": "keyword",
             "to_str": build_kwd_str,
             "verify": verify_node_as_atom}
 
 @composite
 def unqualified_keyword_items(draw):
-    a_kwd_str = draw(unqualified_keyword_as_str())
+    kwd_str = draw(unqualified_keyword_as_str())
     #
-    return {"inputs": a_kwd_str,
+    return {"inputs": kwd_str,
             "label": "keyword",
             "to_str": build_kwd_str,
             "verify": verify_node_as_atom}
 
 @composite
 def qualified_auto_resolved_keyword_items(draw):
-    a_kwd_str = draw(qualified_auto_resolved_keyword_as_str())
+    kwd_str = draw(qualified_auto_resolved_keyword_as_str())
     #
-    return {"inputs": a_kwd_str,
+    return {"inputs": kwd_str,
             "label": "keyword",
             "to_str": build_kwd_str,
             "verify": verify_node_as_atom}
 
 @composite
 def qualified_keyword_items(draw):
-    a_kwd_str = draw(qualified_keyword_as_str())
+    kwd_str = draw(qualified_keyword_as_str())
     #
-    return {"inputs": a_kwd_str,
+    return {"inputs": kwd_str,
             "label": "keyword",
             "to_str": build_kwd_str,
             "verify": verify_node_as_atom}
 
 @composite
 def keyword_items(draw):
-    a_kwd_item = draw(one_of(unqualified_auto_resolved_keyword_items(),
-                             unqualified_keyword_items(),
-                             qualified_auto_resolved_keyword_items(),
-                             qualified_keyword_items()))
-    return a_kwd_item
+    kwd_item = draw(one_of(unqualified_auto_resolved_keyword_items(),
+                           unqualified_keyword_items(),
+                           qualified_auto_resolved_keyword_items(),
+                           qualified_keyword_items()))
+    return kwd_item
