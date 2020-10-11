fac11d9de94a1fe1ceec713076397fa89093a167
sogaiu <983021772@users.noreply.github.com>
2020-10-11 22:07:53 +0900

    Remove stale comment

--- hypothesis_grammar_tree_sitter_clojure/verify.py
+++ hypothesis_grammar_tree_sitter_clojure/verify.py
@@ -118,11 +118,6 @@ def verify_node_metadatum(ctx, item):
 
 def verify_node_metadata(ctx, item):
     node, source = itemgetter('node', 'source')(ctx)
-    # XXX: redundant, but doesn't work sometimes and may be tree-sitter bug
-    #      happened with deref_form_with_metadata_items() strategy
-#    md_node = node.child_by_field_name("metadata")
-#    assert md_node, \
-#      f'no metadata found: {node.sexp()}, {source}'
     md_nodes = child_nodes_with_field_names(node,
                                             ["metadata", "old_metadata"])
     n_md_nodes = len(md_nodes)
