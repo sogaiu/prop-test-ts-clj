efd02c625fcf6cfafeed92e0094d324ba8678e21
sogaiu <983021772@users.noreply.github.com>
2020-10-15 18:54:02 +0900

    Use settings profile

--- prop-test-ts-clj.py
+++ prop-test-ts-clj.py
@@ -45,10 +45,14 @@ from hypothesis_grammar_tree_sitter_clojure.discard_exprs import *
 #
 from hypothesis_grammar_tree_sitter_clojure.forms import *
 
-vb = Verbosity.verbose
-#vb = Verbosity.normal
-
 from tree_sitter import Language, Parser
+
+settings.register_profile("basic",
+                          parent=None,
+                          verbosity=Verbosity.verbose,
+                          suppress_health_check=[HealthCheck.too_slow,
+                                                 HealthCheck.filter_too_much])
+settings.load_profile("basic")
 
 # may need to create build subdir...
 so_with_langs_path = 'build/my-languages.so'
@@ -82,400 +86,304 @@ def form_test(item):
 
 ## comments
 
-@settings(verbosity=vb)
 @given(comment_items())
 def test_parses_comment(comment_item):
     form_test(comment_item)
 
 ## nils
 
-@settings(verbosity=vb)
 @given(nil_items())
 def test_parses_nil(nil_item):
     form_test(nil_item)
 
 ## booleans
 
-@settings(verbosity=vb)
 @given(boolean_items())
 def test_parses_boolean(boolean_item):
     form_test(boolean_item)
 
 ## numbers
 
-@settings(verbosity=vb)
 @given(hex_number_items())
 def test_parses_hex_as_number(hex_num_item):
     form_test(hex_num_item)
 
-@settings(verbosity=vb)
 @given(octal_number_items())
 def test_parses_octal_as_number(oct_num_item):
     form_test(oct_num_item)
 
-@settings(verbosity=vb)
 @given(radix_number_items())
 def test_parses_radix_as_number(radix_num_item):
     form_test(radix_num_item)
 
-@settings(verbosity=vb)
 @given(ratio_items())
 def test_parses_ratio_as_number(ratio_item):
     form_test(ratio_item)
 
-@settings(verbosity=vb)
 @given(double_items())
 def test_parses_double_as_number(double_item):
     form_test(double_item)
 
-@settings(verbosity=vb)
 @given(integer_items())
 def test_parses_integer_as_number(integer_item):
     form_test(integer_item)
 
-@settings(verbosity=vb)
 @given(number_items())
 def test_parses_number(number_item):
     form_test(number_item)
 
 ## symbols
 
-@settings(verbosity=vb)
 @given(unqualified_symbol_items())
 def test_parses_unqualified_symbol(unqual_symbol_item):
     form_test(unqual_symbol_item)
 
-@settings(verbosity=vb)
 @given(qualified_symbol_items())
 def test_parses_qualified_symbol(qual_symbol_item):
     form_test(qual_symbol_item)
 
-@settings(verbosity=vb)
 @given(symbol_items())
 def test_parses_symbol(symbol_item):
     form_test(symbol_item)
 
 ## keywords
 
-@settings(verbosity=vb)
 @given(unqualified_keyword_items())
 def test_parses_unqualified_keyword(unqual_keyword_item):
     form_test(unqual_keyword_item)
 
-@settings(verbosity=vb)
 @given(qualified_keyword_items())
 def test_parses_qualified_keyword(qual_keyword_item):
     form_test(qual_keyword_item)
 
-@settings(verbosity=vb)
 @given(unqualified_auto_resolved_keyword_items())
 def test_parses_unqualified_auto_resolved_keyword(unqual_auto_res_keyword_item):
     form_test(unqual_auto_res_keyword_item)
 
-@settings(verbosity=vb)
 @given(qualified_auto_resolved_keyword_items())
 def test_parses_qualified_auto_resolved_keyword(qual_auto_res_keyword_item):
     form_test(qual_auto_res_keyword_item)
 
-@settings(verbosity=vb)
 @given(keyword_items())
 def test_parses_keyword(keyword_item):
     form_test(keyword_item)
 
 ## characters
 
-@settings(verbosity=vb)
 @given(any_character_items())
 def test_parses_any_character(any_character_item):
     form_test(any_character_item)
 
-@settings(verbosity=vb)
 @given(named_character_items())
 def test_parses_named_character(named_character_item):
     form_test(named_character_item)
 
-@settings(verbosity=vb)
 @given(octal_character_items())
 def test_parses_octal_character(octal_character_item):
     form_test(octal_character_item)
 
-@settings(verbosity=vb)
 @given(unicode_quad_character_items())
 def test_parses_unicode_quad_character(unicode_quad_character_item):
     form_test(unicode_quad_character_item)
 
-@settings(verbosity=vb)
 @given(character_items())
 def test_parses_character(character_item):
     form_test(character_item)
 
 ## strings
 
-@settings(verbosity=vb)
 @given(string_items())
 def test_parses_string(string_item):
     form_test(string_item)
 
 ## symbolic values
 
-@settings(verbosity=vb)
 @given(symbolic_value_items())
 def test_parses_symbolic_value(symbolic_value_item):
     form_test(symbolic_value_item)
 
 ## regex
 
-@settings(verbosity=vb)
 @given(regex_items())
 def test_parses_regex(regex_item):
     form_test(regex_item)
 
 ## atoms
 
-@settings(verbosity=vb)
 @given(atom_items())
 def test_parses_atom(atom_item):
     form_test(atom_item)
 
 ## lists
 
-#@settings(verbosity=vb, suppress_health_check=HealthCheck.all())
-
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(list_items())
 def test_parses_list(list_item):
     form_test(list_item)
 
 ## vectors
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(vector_items())
 def test_parses_vector(vector_item):
     form_test(vector_item)
 
 ## maps
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(map_items())
 def test_parses_map(map_item):
     form_test(map_item)
 
 ## namespaced maps
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(namespaced_map_items())
 def test_parses_namespaced_map(namespaced_map_item):
     form_test(namespaced_map_item)
 
 ## sets
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(set_items())
 def test_parses_set(set_item):
     form_test(set_item)
 
 ## collections
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(collection_items())
 def test_parses_collection(collection_item):
     form_test(collection_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(recursive_collection_items())
 def test_parses_recursive_collection(rec_coll_item):
     form_test(rec_coll_item)
 
 ## reader conditionals
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(read_cond_items())
 def test_parses_reader_conditional(read_cond_item):
     form_test(read_cond_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(read_cond_splicing_items())
 def test_parses_reader_conditional_splicing(read_cond_splicing_item):
     form_test(read_cond_splicing_item)
 
 ## anonymous functions
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(anon_func_items())
 def test_parses_anonymous_function(anon_func_item):
     form_test(anon_func_item)
 
 ## adorned forms
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow])
 @given(deref_form_items())
 def test_parses_deref_form(deref_form_item):
     form_test(deref_form_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow])
 @given(var_quote_form_items())
 def test_parses_var_quote_form(var_quote_form_item):
     form_test(var_quote_form_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow])
 @given(eval_form_items())
 def test_parses_eval_form(eval_form_item):
     form_test(eval_form_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow])
 @given(quote_form_items())
 def test_parses_quote_form(quote_form_item):
     form_test(quote_form_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow])
 @given(syntax_quote_form_items())
 def test_parses_syntax_quote_form(syntax_quote_form_item):
     form_test(syntax_quote_form_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow])
 @given(unquote_form_items())
 def test_parses_unquote_form(unquote_form_item):
     form_test(unquote_form_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow])
 @given(unquote_splicing_form_items())
 def test_parses_unquote_splicing_form(unquote_splicing_form_item):
     form_test(unquote_splicing_form_item)
 
 ## tagged literals
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(tagged_literal_items())
 def test_parses_tagged_literal(tagged_literal_item):
     form_test(tagged_literal_item)
 
 ## metadata
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(symbol_items(metadata="any"))
 def test_parses_symbol_with_metadata(symbol_with_metadata_item):
     form_test(symbol_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(list_items(elements=atom_items(), metadata="any"))
 def test_parses_atom_list_with_metadata(atom_list_with_metadata_item):
     form_test(atom_list_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(map_items(elements=atom_items(), metadata="any"))
 def test_parses_atom_map_with_metadata(atom_map_with_metadata_item):
     form_test(atom_map_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(namespaced_map_items(elements=atom_items(), metadata="any"))
 def test_parses_atom_namespaced_map_with_metadata(atom_namespaced_map_with_metadata_item):
     form_test(atom_namespaced_map_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(set_items(elements=atom_items(), metadata="any"))
 def test_parses_atom_set_with_metadata(atom_set_with_metadata_item):
     form_test(atom_set_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(vector_items(elements=atom_items(), metadata="any"))
 def test_parses_atom_vector_with_metadata(atom_vector_with_metadata_item):
     form_test(atom_vector_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(read_cond_items(metadata="any"))
 def test_parses_reader_conditional_with_metadata(read_cond_with_metadata_item):
     form_test(read_cond_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(read_cond_splicing_items(metadata="any"))
 def test_parses_reader_conditional_splicing_with_metadata(read_cond_splicing_with_metadata_item):
     form_test(read_cond_splicing_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(anon_func_items(metadata="any"))
 def test_parses_anonymous_function_with_metadata(anon_func_with_metadata_item):
     form_test(anon_func_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(deref_form_items(metadata="any"))
 def test_parses_deref_form_with_metadata(deref_form_with_metadata_item):
     form_test(deref_form_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(eval_form_items(metadata="any"))
 def test_parses_eval_form_with_metadata(eval_form_with_metadata_item):
     form_test(eval_form_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(quote_form_items(metadata="any"))
 def test_parses_quote_form_with_metadata(quote_form_with_metadata_item):
     form_test(quote_form_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(syntax_quote_form_items(metadata="any"))
 def test_parses_syntax_quote_form_with_metadata(syntax_quote_form_with_metadata_item):
     form_test(syntax_quote_form_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(unquote_form_items(metadata="any"))
 def test_parses_unquote_form_with_metadata(unquote_form_with_metadata_item):
     form_test(unquote_form_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(unquote_splicing_form_items(metadata="any"))
 def test_parses_unquote_splicing_form_with_metadata(unquote_splicing_form_with_metadata_item):
     form_test(unquote_splicing_form_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(var_quote_form_items(metadata="any"))
 def test_parses_var_quote_form_with_metadata(var_quote_form_with_metadata_item):
     form_test(var_quote_form_with_metadata_item)
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(tagged_literal_items(metadata="any"))
 def test_parses_tagged_literal_with_metadata(tagged_literal_with_metadata_item):
     form_test(tagged_literal_with_metadata_item)
 
 ## discard expressions
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(discard_expr_items())
 def test_parses_discard_expr(discard_expr_item):
     form_test(discard_expr_item)
 
 ## forms
 
-@settings(verbosity=vb, suppress_health_check=[HealthCheck.too_slow,
-                                               HealthCheck.filter_too_much])
 @given(form_items())
 def test_parses_form(form_item):
     form_test(form_item)
